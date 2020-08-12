c43b6cdb23a2604f7202af8a6b873e14113b25ec
nurbolatk <nurbolattk@gmail.com>
2020-08-12 23:13:27 +0600

    import react router

--- src/components/molecules/Content.js
+++ src/components/molecules/Content.js
@@ -1,52 +1,7 @@
 import React from 'react'
 
 const Content = () => {
-  return (
-    <div className='content'>
-      <div className='card' style={{ backgroundColor: '#22252a' }}>
-        aloha
-      </div>
-      <div className='card' style={{ backgroundColor: '#2d3035' }}>
-        aloha
-      </div>
-      <div className='card' style={{ backgroundColor: '#2f3237' }}>
-        aloha
-      </div>
-      <div className='card' style={{ backgroundColor: '#45484c' }}>
-        aloha
-      </div>
-      <div className='card' style={{ backgroundColor: '#76787b' }}>
-        aloha
-      </div>
-      <div className='card' style={{ backgroundColor: '#7f8083' }}>
-        aloha
-      </div>
-      <div className='card' style={{ backgroundColor: '#999999' }}>
-        aloha
-      </div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-    </div>
-  )
+  return <div className='content'></div>
 }
 
 export default Content
