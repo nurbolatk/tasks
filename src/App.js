6fd7b5321637210854176515d92bc2cc8c53640f
nurbolatk <nurbolattk@gmail.com>
2020-08-12 20:38:50 +0600

    Create main container layouts

--- src/App.js
+++ src/App.js
@@ -1,58 +1,17 @@
 import React from 'react'
-import api from './api'
 import './sass/main.scss'
+import Topbar from './components/organisms/Topbar'
+import Sidebar from './components/organisms/Sidebar'
+import Content from './components/molecules/Content'
+import Menu from './components/organisms/Menu'
 
 const App = () => {
-  const [todo, setTodo] = React.useState('')
-  const [todos, setTodos] = React.useState([])
-  const [isLoading, setIsLoading] = React.useState(true)
-
-  React.useEffect(() => {
-    const getTodos = async () => {
-      try {
-        const res = await api.get('/todos')
-        setIsLoading(false)
-        setTodos(res.data)
-      } catch (err) {
-        setIsLoading(false)
-        alert(err.message)
-      }
-    }
-    getTodos()
-  }, [])
-
-  const saveTodo = e => {
-    e.preventDefault()
-    const apiPromise = api.post('/todos', todo)
-
-    apiPromise
-      .then(data => {
-        console.log(data)
-        // setTodo('')
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }
-
   return (
-    <div>
-      <h1>hello there</h1>
-      <form onSubmit={saveTodo}>
-        <input
-          type='text'
-          value={todo}
-          onChange={e => setTodo(e.target.value)}
-        />
-        <button>Save</button>
-      </form>
-      <ul>
-        {isLoading ? (
-          <p>Loading...</p>
-        ) : (
-          todos.map(todo => <li key={todo.text}>{todo.text}</li>)
-        )}
-      </ul>
+    <div className='app'>
+      <Topbar />
+      <Sidebar />
+      <Menu />
+      <Content />
     </div>
   )
 }
---
+++ src/components/molecules/Content.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div className='content'>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+      <div className='card'>aloha</div>
+    </div>
+  )
+}
+
+export default Content
