cf421d97a537bbd9e95fa11ac37260b41140318b
nurbolatk <nurbolattk@gmail.com>
2020-08-12 21:05:13 +0600

    Add main colors to variables.scss

--- src/components/molecules/Content.js
+++ src/components/molecules/Content.js
@@ -3,13 +3,27 @@ import React from 'react'
 const Content = () => {
   return (
     <div className='content'>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
-      <div className='card'>aloha</div>
+      <div className='card' style={{ backgroundColor: '#22252a' }}>
+        aloha
+      </div>
+      <div className='card' style={{ backgroundColor: '#2d3035' }}>
+        aloha
+      </div>
+      <div className='card' style={{ backgroundColor: '#2f3237' }}>
+        aloha
+      </div>
+      <div className='card' style={{ backgroundColor: '#45484c' }}>
+        aloha
+      </div>
+      <div className='card' style={{ backgroundColor: '#76787b' }}>
+        aloha
+      </div>
+      <div className='card' style={{ backgroundColor: '#7f8083' }}>
+        aloha
+      </div>
+      <div className='card' style={{ backgroundColor: '#999999' }}>
+        aloha
+      </div>
       <div className='card'>aloha</div>
       <div className='card'>aloha</div>
       <div className='card'>aloha</div>
