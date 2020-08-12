cc2158dab73f0226d270168cbf5b70c704a08640
nurbolatk <nurbolattk@gmail.com>
2020-08-12 22:45:31 +0600

    Style primary button and some more stuff

--- src/components/organisms/Menu.js
+++ src/components/organisms/Menu.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const Menu = () => {
-  return <div className='menu'>menu</div>
+  return (
+    <div className='menu'>
+      <div className='p-3'>
+        <button className='btn btn-primary w-100'>Add task</button>
+      </div>
+    </div>
+  )
 }
 
 export default Menu
