26c808fd6c8204bd4ef2589ead74dcc5de633bde
nurbolatk <nurbolattk@gmail.com>
2020-09-05 22:28:14 +0600

    Downgrage to right version history and create global history object

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import classnames from 'classnames'
 import './sass/main.scss'
 import Topbar from './components/organisms/Topbar'
 import Sidebar from './components/organisms/Sidebar'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Router, Switch, Route } from 'react-router-dom'
 import Books from './components/pages/Books'
 import Tasks from './features/tasks/Tasks'
 import Projects from './features/projects/Projects'
 import TaskFull from './features/tasks/TaskFull'
 import { useSelector } from 'react-redux'
 import { selectCurrentTask } from './features/tasks/tasksSlice'
+import history from './utils/history'
 
 const App = () => {
   const currentTask = useSelector(selectCurrentTask)
   return (
-    <Router>
+    <Router history={history}>
       <div
         className={classnames('app', {
           'app-right-sidebar-open': currentTask,
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { Provider } from 'react-redux'
 import store from './store'
-import { createBrowserHistory } from 'history'
-console.log('createBrowserHistory', createBrowserHistory)
 
 ReactDOM.render(
   <Provider store={store}>
