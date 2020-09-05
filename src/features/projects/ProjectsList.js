5453aa4cc0193abebdb9bde89277cf50a3ac3f7b
nurbolatk <nurbolattk@gmail.com>
2020-09-05 22:41:00 +0600

    Listen for history updates in ProjectList instead of storing path in redux

--- src/features/projects/ProjectsList.js
+++ src/features/projects/ProjectsList.js
@@ -1,18 +1,31 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchProjects, selectCurrentProject } from './projectsSlice'
-import { useHistory } from 'react-router-dom'
+import { fetchProjects } from './projectsSlice'
 import Menu from '../../components/organisms/Menu'
+
+import history from './../../utils/history'
+import { useState } from 'react'
 
 const ProjectsList = () => {
   const projects = useSelector(state => state.projects.projects)
-  const currentProjectId = useSelector(selectCurrentProject)
+  const [currentProjectId, setCurrentProjectId] = useState(
+    history.location.pathname.split('/').slice(-1).pop()
+  )
+  // const currentProjectId = useSelector(selectCurrentProject)
   const dispatch = useDispatch()
-  const history = useHistory()
 
   useEffect(() => {
     dispatch(fetchProjects())
   }, [dispatch])
+
+  useEffect(() => {
+    const unlisten = history.listen((location, action) => {
+      // location is an object like window.location
+      const path = location.pathname.split('/')
+      setCurrentProjectId(path[path.length - 1])
+    })
+    return unlisten
+  }, [])
 
   const chooseProject = project => {
     if (currentProjectId !== project.id) {
